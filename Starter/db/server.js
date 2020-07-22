d136a219cdd18a260e0df65b37ea278e43fcdc70
Ian Martinez <imartinez0753@gmail.com>
2020-07-22 13:21:29 -0700

    Post

--- Starter/db/server.js
+++ Starter/db/server.js
@@ -5,12 +5,56 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//gets
 //----------------------------------------------------------
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/../public/index.html"));
 });
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/../public/notes.html"));
+});
+// api
+//------------------------------------------------------------
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db.json", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(data);
+    return res.json(data);
+  });
+  //   return res.readFile(path("db.json"));
+  // return res.json(db.json);
+  //   return res.fs.readFile("db.json", "utf8");
+});
+//post
+//-------------------------------------------
+app.post("/api/notes", function (req, res) {
+  var newEntry = req.body;
+  fs.appendFile("db.json", newEntry + "\n", function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Commit logged!");
+      res.json(newEntry);
+    }
+  });
+});
+
+//   var newTable = req.body;
+
+//   console.log(newTable);
+
+//   waitList.push(newTable);
+
+//   res.json(newTable);
+// });
+//delete
+//-----------------------------------------------------
+app.post("/api/delete", function (req, res) {
+  tables.length = 0;
+  waitList.length = 0;
+  res.json({ ok: true });
 });
 //----------------------------------------------------------
 app.listen(PORT, function () {
